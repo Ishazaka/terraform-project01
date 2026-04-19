3a2bbeae315328c28bda5ef8b71774f3043117ce
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2026-04-19 01:08:04 -0400

    Fixed security group reference in aws_instance resource

--- 1st-version/main.tf
+++ 1st-version/main.tf
@@ -137,7 +137,7 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.web_sg]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name               = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
