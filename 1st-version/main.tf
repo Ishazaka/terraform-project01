8cb28baaefcb0100e0cc36f72c0e2c29163c725b
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2026-04-19 00:11:29 -0400

    AWS infrastructure with Terraform
    This Terraform configuration sets up an AWS infrastructure including a VPC, public subnet, internet gateway, route table, security group, key pair, and an EC2 instance with a web server.

---
+++ 1st-version/main.tf
@@ -0,0 +1,157 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
+locals {
+  common_tags = {
+    Project    = var.project_name
+    Managed_By = "Terraform"
+  }
+}
+
+# creating vpc 
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-vpc"
+  })
+}
+
+# public subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-public-subnet"
+  })
+}
+
+
+# Create internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-igw"
+  })
+}
+
+
+# create route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-public-rt"
+  })
+}
+
+
+// associate route table with subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+
+}
+
+
+# Create security group
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    to_port     = 22
+    from_port   = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    to_port     = 80
+    from_port   = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    to_port     = 0
+    from_port   = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-sg"
+  })
+
+}
+
+#data source to get the latest AMI ID of Instance 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+# create keypair
+resource "aws_key_pair" "deployer" {
+  key_name   = "${var.project_name}-key"
+  public_key = file(var.public_key_path)
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-key"
+  })
+}
+
+
+# creating aws ec2 instance 
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg]
+  key_name               = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              echo "<h1>Hello from Terraform EC2</h1>" > /var/www/html/index.html
+              EOF
+
+tags = merge(local.common_tags, {
+    Name = "${var.project_name}-ec2"
+})
+
+}
